47af562107e3326484d15a1bb757f5a93b8c9e02
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 18:29:55 -0500

    clase math la funcion es interesante

---
+++ src/match.java
@@ -0,0 +1,26 @@
+
+public class match {
+
+	public static void main(String[] args) {
+		
+		//double raiz = Math.sqrt(36.77);saca la raiz cuadrada
+		
+		double base = 5, exponente = 2;
+		double resultado = Math.pow(base,exponente);//es al cuadrado al numero bas al exponente
+		System.out.println(resultado);
+		
+		//--------------------------------REDONDEAR UN NUMERO-----
+		
+		float numero = 4.56f;
+		int result = Math.round(numero);//redondea el numero a 6
+		System.out.println(result);
+		//----------------------------metodo ramdom
+		
+		
+		double resulta = Math.random();//un valor tipo double numero aleatorio
+		System.out.println(resulta);
+		
+		
+	}
+
+}
