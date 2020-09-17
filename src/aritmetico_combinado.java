0e1414b24478c9c25350e1e41e2524f83eff244e
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 17:54:35 -0500

    aritmetico combinado

---
+++ src/aritmetico_combinado.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class aritmetico_combinado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		int numero = 10;
+		
+		numero += 5;//sea suma resta o multiplicacion es lo mismo
+		
+		System.out.println("el numero es "+numero);
+		
+		
+		
+		
+		
+	}
+
+}
