ec7b97afac91b6739a8be0dc112725f515530589
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 17:57:20 -0500

    opcion incremento decremento

---
+++ src/incremento_decremento.java
@@ -0,0 +1,28 @@
+
+public class incremento_decremento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		
+		x ++;//equivale x + 1
+		
+		System.out.println(x);//al 5 aumentaa uno serian 6
+		//----------------------------------------------------decremento
+		int y = 5;
+		
+		y --;//equivale x + 1
+		
+		System.out.println(x);
+		//--------------------------------EJERCICIO----------------------------
+		
+		int o = 5,p;
+		
+		p = o++;
+		
+		System.out.println("p");//la salida es 5
+		System.out.println("o");//vale 6
+	}
+
+}
